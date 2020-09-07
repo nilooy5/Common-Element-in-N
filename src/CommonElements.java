88fa9f0dc228c4dbb23cac350b67bc689f581494
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-09-08 04:53:43 +0600

    print statement

--- src/CommonElements.java
+++ src/CommonElements.java
@@ -2,6 +2,9 @@ import java.util.Arrays;
 
 public class CommonElements {
     public static void main(String[] args) {
+        System.out.println("TEST PERFORMED by Munira Tabassum");
+        System.out.println("TEST for Common Elements between two Arrays in N time");
+
         int[] arr1 = { 53, 39, 84, 105, 42, 96 };
         int[] arr2 = { 96, 33, 105, 47, 75, 35, 13, 39};
         PrintCommons(arr1,arr2);
