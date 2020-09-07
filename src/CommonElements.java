d08c88fb9d9543c35b3f9372cbad4f4911ab08cd
Fazal Mahmud Niloy <niloooy@gmail.com>
2020-09-08 04:50:59 +0600

    initial commit

---
+++ src/CommonElements.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class CommonElements {
+    public static void main(String[] args) {
+        int[] arr1 = { 53, 39, 84, 105, 42, 96 };
+        int[] arr2 = { 96, 33, 105, 47, 75, 35, 13, 39};
+        PrintCommons(arr1,arr2);
+    }
+    private static void PrintCommons(int[] arr1, int[] arr2) {
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int a = 0;
+        int b = 0;
+        while ((a < arr1.length) && (b < arr2.length)) {
+            if (arr1[a] < arr2[b]) a++;
+            else if (arr2[b] < arr1[a]) b++;
+            else {
+                System.out.println(arr2[b]);
+                a++;
+                b++;
+            }
+        }
+    }
+}
